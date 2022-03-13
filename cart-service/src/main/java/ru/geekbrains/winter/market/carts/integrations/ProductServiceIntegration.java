70e73bb3bd017ce2249449edad405f9f9d905ee8
julia <89081314528@mail.ru>
2022-03-13 15:02:02 +0300

    lesson5

--- cart-service/src/main/java/ru/geekbrains/winter/market/carts/integrations/ProductServiceIntegration.java
+++ cart-service/src/main/java/ru/geekbrains/winter/market/carts/integrations/ProductServiceIntegration.java
@@ -12,22 +12,13 @@ public class ProductServiceIntegration {
     private final RestTemplate restTemplate;
 
     @Value("${url.getProductById}")
-//    private String urlGetProductById = getProductById(null).toString(); // можно вызвать метод, который что то возвращает
-    // sout вызвать нельзя
-    // можно создать поле, инициализировать его, объявить метод
-    // вызвать метод можно только в анонимном блоке инициализации
     private String urlGetProductById;
-//    {
-//        System.out.println(urlGetProductById); // анонимный блок инициализации, редко используется
-//    }
     public ProductServiceIntegration(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
 
     public Optional<ProductDto> getProductById(Long id) {
-//        return Optional.ofNullable(restTemplate.getForObject("http://localhost:8189/winter/api/v1/products/" + id, ProductDto.class));
-        System.out.println("orange");
         System.out.println(urlGetProductById);
         return Optional.ofNullable(restTemplate.getForObject(urlGetProductById + id, ProductDto.class));
     }
