fd4d89f7f97b38aef24ebdc7d84b8673a862f867
julia <89081314528@mail.ru>
2022-03-16 20:39:02 +0300

    lesson6

---
+++ core-service/src/main/java/ru/geekbrains/winter/market/core/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.winter.market.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.winter.market.core.entities.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> getByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal min, BigDecimal max);
+
+}
